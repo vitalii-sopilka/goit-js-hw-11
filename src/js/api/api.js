d4ed7d3d61377ba2446d26dc6964aa51f4741bd6
Vitalii <33999219+vitalii-sopilka@users.noreply.github.com>
2023-06-24 10:26:48 +0200

    hw11-v03

--- src/js/api/api.js
+++ src/js/api/api.js
@@ -1,20 +1,19 @@
 import axios from "axios";
 
 export async function getPhotos(inputValue, page) {
-    try {
-      return await axios.get('https://pixabay.com/api/', {
-        params: {
-            key: '37265508-698720a89f242e7ec4ebdb49a',
-            q: inputValue,
-            image_type: 'photo',
-            orientation: 'horizontal',
-            safesearch : true,
-            per_page: 40,
-            page: page,
-        }
-      });
-      
-    } catch (error) {
-      console.log(error);
-    }
+  try {
+    return await axios.get('https://pixabay.com/api/', {
+      params: {
+          key: '37775018-35716a84a46a948528041aaab',
+          q: inputValue,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch : true,
+          per_page: 40,
+          page: page,
+      }
+    });
+  } catch (error) {
+    console.log(error);
   }
+}
